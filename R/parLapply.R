ed0468b27f54744ab2822a2d33ecf49239302263
Roger D. Peng [amelia] <rpeng@jhsph.edu>
2008-08-04 18:25:01 -0400

    Have plapply share the function and put results in a separate DB

--- R/parLapply.R
+++ R/parLapply.R
@@ -1,34 +1,52 @@
 library(filehash)
 
-plapply <- function(X, FUN, ...) {
-        dbl <- makeQ(X)
-        print(dbl$name)
+plapply <- function(X, FUN, name = NULL) {
+        ## Make shared "memory" queue
+        if(is.null(name))
+                name <- filehash:::sha1(X)
+        dbl <- createQ(name)
         putQ(dbl, X)
+
+        ## A queue for the data 'X'
+        rdbname <- paste(dbl$name, "result", sep = ".")
+        rdb <- createQ(rdbname)
+
+        ## Share the function 'FUN'
+        con <- file(paste(dbl$name, "FUN", sep = "."), "wb")
+        serialize(FUN, con)
+        close(con)
         
         repeat {
                 while(inherits(obj <- try(popQ(dbl)), "try-error"))
                         next
                 if(is.null(obj))
                         break
-                FUN(obj, ...)
+                result <- FUN(obj)
+
+                while(inherits(try(putQ(rdb, result)), "try-error"))
+                        next
                 Sys.sleep(0.5)
         }
 }
 
-plapply2 <- function(name, FUN, ...) {
+worker <- function(name) {
         dbl <- initQ(name)
+        rdbname <- paste(dbl$name, "result", sep = ".")
+        rdb <- initQ(rdbname)
+
+        con <- file(paste(dbl$name, "FUN", sep = "."), "rb")
+        FUN <- unserialize(con)
+        close(con)
         
         repeat {
                 while(inherits(obj <- try(popQ(dbl)), "try-error"))
                         next
                 if(is.null(obj))
                         break
-                FUN(obj, ...)
+                result <- FUN(obj)
+
+                while(inherits(try(putQ(rdb, result)), "try-error"))
+                        next
                 Sys.sleep(0.5)
         }
-}
-
-makeQ <- function(x) {
-        name <- filehash:::sha1(x)
-        createQ(name)
 }
