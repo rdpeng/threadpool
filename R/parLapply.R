5d00b3253f52655f71f861d1bde5256202b6d1bb
Roger D. Peng [amelia] <rpeng@jhsph.edu>
2008-08-05 01:48:24 -0400

    Add pStack, getFUN, setFUN

--- R/parLapply.R
+++ R/parLapply.R
@@ -1,29 +1,48 @@
 library(filehash)
 
+pStack <- function(name) {
+        ## A stack for the results
+        rdbname <- paste(db$name, "result", sep = ".")
+        list(db = createS(name),
+             rdb = createS(rdbname))
+}
+
 plapply <- function(X, FUN, name = NULL) {
         ## Make "shared memory" stack
         if(is.null(name))
                 name <- filehash:::sha1(X)
-        db <- createS(name)
-        pushS(db, X)
-
-        ## A stack for the results
-        rdbname <- paste(db$name, "result", sep = ".")
-        rdb <- createS(rdbname)
+        p <- pStack(name)
+        mpushS(p$db, X)
 
         ## Share the function via "shared memory"
-        dbInsert(db, "FUN", FUN)
+        setFUN(p$db, FUN)
         
         repeat {
-                while(inherits(obj <- try(popS(db)), "try-error"))
+                while(inherits(obj <- try(popS(p$db)), "try-error"))
                         next
                 if(is.null(obj))
                         break
                 result <- FUN(obj)
 
-                while(inherits(try(putS(rdb, result)), "try-error"))
+                while(inherits(try(pushS(p$rdb, result)), "try-error"))
                         next
                 Sys.sleep(0.5)
+        }
+}
+
+setFUN <- function(db, FUN) {
+        dbInsert(db$stack, "FUN", FUN)
+}
+
+getFUN <- function(db) {
+        dbFetch(db$stack, "FUN")
+}
+
+################################################################################
+
+pollWorker <- function(name) {
+        repeat {
+                try(worker(name), silent = TRUE)
         }
 }
 
@@ -31,7 +50,7 @@ worker <- function(name) {
         db <- initS(name)
         rdbname <- paste(db$name, "result", sep = ".")
         rdb <- initS(rdbname)
-        FUN <- dbFetch(db, "FUN")
+        FUN <- getFUN(db)
 
         if(isEmptyS(db))
                 return(invisible(NULL))
@@ -42,7 +61,7 @@ worker <- function(name) {
                         break
                 result <- FUN(obj)
 
-                while(inherits(try(putS(rdb, result)), "try-error"))
+                while(inherits(try(pushS(rdb, result)), "try-error"))
                         next
                 Sys.sleep(0.5)
         }
