9ca09cf3a707674fabb77998247093cdf86d1e40
Roger D. Peng [audrey] <rdpeng@gmail.com>
2017-08-28 11:07:32 +1000

    Each process has to separately join the cluster

--- R/interface.R
+++ R/interface.R
@@ -1,22 +1,31 @@
 ## Higher level interface
 
 
-#' @importFrom parallel mclapply
+#' @importFrom parallel mcparallel
 #' @export
 #'
 rw_map <- function(x, f, ..., cl_name = NULL, ncores = 2L) {
         x <- as.list(x)
         if(is.null(cl_name))
-                clname <- tempfile("cluster")
+                cl_name <- tempfile("cluster")
+        initialize_cluster_queue(cl_name, x, f, ...)
+        presult <- vector("list", length = ncores)
+        for(i in seq_len(ncores)) {
+                presult[[i]] <- mcparallel({
+                        cl <- cluster_join(cl_name)
+                        cluster_run(cl)
+                })
+        }
+        presult
+}
+
+
+
+initialize_cluster_queue <- function(cl_name, x, f, ...) {
         cl <- cluster_create(cl_name)
         for(i in seq_along(x)) {
                 task <- new_task(x[[i]], f)
                 cluster_add1_task(cl, task)
         }
-        presult <- vector("list", length = ncores)
-        for(i in seq_len(ncores)) {
-                presult[[i]] <- mcparallel(cluster_run(cl))
-        }
-        presult
+        invisible(NULL)
 }
-
--- misc/testscript.R
+++ misc/testscript.R
@@ -3,14 +3,14 @@
 library(rwmodel)
 
 setwd("~/tmp")
-clname <- "cluster1"
-cl <- cluster_create(clname)
-str(cl)
+##clname <- "cluster1"
+##cl <- cluster_create(clname)
+##str(cl)
 
 ## Generate some tasks
 
 set.seed(2017-08-25)
-seconds <- c(1, 3, 10)
+seconds <- c(1, 2, 5)
 probs <- c(0.6, 0.35, 0.05)
 n <- 100
 data <- sample(seconds, n, replace = TRUE, prob = probs)
@@ -33,6 +33,11 @@ process <- function(filename) {
                          basename(filename), "!"),
                    file.path("output", basename(filename)))
 }
+
+trace("rw_map", quote(browser()), at = 7)
+p <- rw_map(taskfiles, process, cl_name = "cluster1", ncores = 4)
+
+
 
 out <- lapply(taskfiles, function(filename) {
         message(filename)
