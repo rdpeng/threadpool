80b8cb9eb58cd0383ff9a58cddba016e47be3cd0
Roger D. Peng [audrey] <rdpeng@gmail.com>
2017-08-28 11:27:02 +1000

    Option for waiting on the result (default = TRUE)

--- R/interface.R
+++ R/interface.R
@@ -1,14 +1,19 @@
 ## Higher level interface
 
-
+#' @param x an R object that will be coerced to a list
+#' @param f a function
+#' @param cl_name an option name for the cluster queue
+#' @param ncores the number of cores to use
+#' @param wait_for_result should we wait for all results to finish? (default = TRUE)
 #' @importFrom parallel mcparallel
 #' @export
 #'
-rw_map <- function(x, f, ..., cl_name = NULL, ncores = 2L) {
+rw_map <- function(x, f, cl_name = NULL, ncores = 2L, wait_for_result = TRUE) {
+        f <- match.fun(f)
         x <- as.list(x)
         if(is.null(cl_name))
                 cl_name <- tempfile("cluster")
-        initialize_cluster_queue(cl_name, x, f, ...)
+        initialize_cluster_queue(cl_name, x, f)
         presult <- vector("list", length = ncores)
         for(i in seq_len(ncores)) {
                 presult[[i]] <- mcparallel({
@@ -16,12 +21,16 @@ rw_map <- function(x, f, ..., cl_name = NULL, ncores = 2L) {
                         cluster_run(cl)
                 })
         }
-        presult
+        if(wait_for_result)
+                rvalue <- mccollect(presult)
+        else
+                rvalue <- presult
+        rvalue
 }
 
 
 
-initialize_cluster_queue <- function(cl_name, x, f, ...) {
+initialize_cluster_queue <- function(cl_name, x, f) {
         cl <- cluster_create(cl_name)
         for(i in seq_along(x)) {
                 task <- new_task(x[[i]], f)
