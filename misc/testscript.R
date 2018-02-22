4c6af9861f1faaeabb04e3f2f76a5a92675d6c5d
Roger D. Peng [audrey] <rdpeng@gmail.com>
2018-02-22 13:30:59 +1100

    Simpler minimal example

--- misc/testscript.R
+++ misc/testscript.R
@@ -1,19 +1,55 @@
 ## Test cluster
 
-library(rwmodel)
+library(threadpool)
+
+
+
 
 setwd("~/tmp")
-##clname <- "cluster1"
-##cl <- cluster_create(clname)
-##str(cl)
+
+cl_name <- "cluster1"
 
 ## Generate some tasks
+n <- 500
+x <- seq_len(n)
+x <- as.list(x)
+f <- function(num, meta) {
+        pid <- Sys.getpid()
+        nr <- nrow(meta)
+        message(pid, " is running task ", num, ": metadata has ", nr, " rows")
+        Sys.sleep(1)
+        list(output = paste0(pid, " is finished running ", num, "!"))
+}
+meta <- airquality
 
-set.seed(2017-08-25)
-seconds <- c(1, 2, 5)
-probs <- c(0.6, 0.35, 0.05)
-n <- 100
-data <- sample(seconds, n, replace = TRUE, prob = probs)
+## Start up cluster
+initialize_cluster_queue(cl_name, x, f, meta = airquality)
+
+cl <- cluster_join(cl_name)
+cluster_run(cl)
+
+
+library(threadpool)
+cl <- cluster_join("cluster1")
+cluster_run(cl)
+
+
+
+delete_cluster(cl_name)
+
+########################################################
+## Initialize cluster by hand
+
+cl <- cluster_create(cl_name)
+for(i in seq_along(x)) {
+        task <- new_task(x[[i]], f)
+        cluster_add1_task(cl, task)
+        ## enqueue(cl$injob, task)
+}
+saveRDS(meta, cl$meta, compress = FALSE)
+
+
+########################################################
 
 dir.create("input")
 taskfiles <- paste0("input/task", formatC(1:n, flag = "0", width = 3))
