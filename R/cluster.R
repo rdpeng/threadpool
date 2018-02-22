85f17bc24ef069b4ee0cdc85886059c80a67ac8f
Roger D. Peng [audrey] <rdpeng@gmail.com>
2018-02-22 13:31:43 +1100

    Flesh out underlying functions

--- R/cluster.R
+++ R/cluster.R
@@ -1,11 +1,28 @@
 
+cluster_paths <- function(path) {
+        list(injob = sprintf("%s.in.q", path),
+             outjob = sprintf("%s.out.q", path),
+             meta = sprintf("%s.meta.rds", path))
+}
+
+#' @export
+#'
+delete_cluster <- function(path) {
+        p <- cluster_paths(path)
+        unlink(p$injob, recursive = TRUE)
+        unlink(p$outjob, recursive = TRUE)
+        file.remove(p$meta)
+}
+
 #' @importFrom queue create_queue
 #' @export
 #'
 cluster_create <- function(path) {
-        cl <- list(injob = create_queue(sprintf("%s.in.q", path)),
-                   outjob = create_queue(sprintf("%s.out.q", path)),
-                   path = path)
+        p <- cluster_paths(path)
+        cl <- list(injob = create_queue(p$injob),
+                     outjob = create_queue(p$outjob),
+                     meta = p$meta,
+                     path = path)
         cl
 }
 
@@ -13,8 +30,10 @@ cluster_create <- function(path) {
 #' @export
 #'
 cluster_join <- function(path) {
-        cl <- list(injob = init_queue(sprintf("%s.in.q", path)),
-                   outjob = create_queue(sprintf("%s.out.q", path)),
+        p <- cluster_paths(path)
+        cl <- list(injob = init_queue(p$injob),
+                   outjob = init_queue(p$outjob),
+                   meta = p$meta,
                    path = path)
         cl
 }
@@ -29,13 +48,16 @@ new_task <- function(data, func) {
 #' @export
 #'
 cluster_add1_task <- function(cl, task) {
-        enqueue(cl$injob, task)
+        injob_q <- cl$injob
+        enqueue(injob_q, task)
 }
 
 #' @importFrom queue dequeue
+#' @export
 #'
 cluster_next_task <- function(cl) {
-        task <- dequeue(cl$injob)
+        injob_q <- cl$injob
+        task <- try(dequeue(injob_q), silent = TRUE)
         task
 }
 
@@ -44,15 +66,19 @@ cluster_next_task <- function(cl) {
 #' @importFrom queue is_empty
 #'
 cluster_run <- function(cl) {
-        while(!is_empty(cl$injob)) {
-                task <- cluster_next_task(cl)
-                task_run(task)
+        meta <- readRDS(cl$meta)
+
+        while(!inherits(task <- cluster_next_task(cl), "try-error")) {
+                result <- task_run(task, meta)
+                output <- task_output(result)
+                cluster_finish_task(cl, output)
         }
         invisible(NULL)
 }
 
-task_run <- function(task) {
-        result <- with(task, func(data))
+task_run <- function(task, meta) {
+        result <- with(task, do.call(func, list(data, meta)))
+        ## result <- with(task, func(data, meta))
         result
 }
 
