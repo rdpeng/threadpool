96872e7034a3092e8e11c57a767ac757ae81aff0
Roger D. Peng [audrey] <rdpeng@gmail.com>
2018-06-07 13:07:05 +1000

    Let's not use tp_map for now

--- inst/misc/testscript.R
+++ inst/misc/testscript.R
@@ -1,34 +1,4 @@
 ########################################################
-## Use tp_map() function
-
-setwd("~/tmp")
-dir()
-
-## Generate some tasks
-n <- 500
-x <- seq_len(n)
-x <- as.list(x)
-f <- function(num, meta) {
-        pid <- Sys.getpid()
-        outfile <- sprintf("output_%d.txt", pid)
-        nr <- nrow(meta)
-        cat(pid, "is running task", num, ": metadata has", nr, "rows\n",
-            file = outfile, append = TRUE)
-        Sys.sleep(1)
-        list(output = paste0(pid, " is finished running ", num, "!"))
-}
-meta <- airquality
-cl_name <- "cluster1"
-
-tp_map(x, f, cl_name, meta = meta, ncores = 3)
-
-delete_cluster(cl_name)
-
-
-
-
-########################################################
-
 ## Test cluster
 
 library(threadpool)
@@ -42,21 +12,17 @@ cl_name <- "cluster1"
 n <- 500
 x <- seq_len(n)
 x <- as.list(x)
-f <- function(num, meta) {
+f <- function(num) {
         pid <- Sys.getpid()
-        nr <- nrow(meta)
-        cat("PID ", pid, " is running task ", num, ": metadata has ", nr,
-            " rows\n", file = "output.log", append = TRUE)
+        cat("PID ", pid, " is running task ", num, "\n",
+            file = "output.log", append = TRUE)
         Sys.sleep(1)
         list(output = paste0(pid, " is finished running ", num, "!"))
 }
-meta <- airquality
 
 ## Start up cluster
-initialize_cluster_queue(cl_name, x, f, env = globalenv(),
-                         meta = airquality, mapsize = NULL)
-
-cl <- cluster_join(cl_name, 2^30)
+initialize_cluster_queue(cl_name, x, f, env = globalenv())
+cl <- cluster_join(cl_name)
 cluster_run(cl)
 
 
@@ -66,17 +32,6 @@ cluster_add_nodes(cl_name, 3)
 delete_cluster(cl_name)
 
 
-
-########################################################
-## Initialize cluster by hand
-
-cl <- cluster_create(cl_name)
-for(i in seq_along(x)) {
-        task <- new_task(x[[i]], f)
-        cluster_add1_task(cl, task)
-        ## enqueue(cl$injob, task)
-}
-saveRDS(meta, cl$meta, compress = FALSE)
 
 
 ########################################################
