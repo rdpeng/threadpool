231cd961fb7f51ee7102a633edacc601e976ca57
Roger D. Peng <rdpeng@gmail.com>
2018-06-11 14:45:33 +1000

    Testing!!!

--- inst/misc/testscript.R
+++ inst/misc/testscript.R
@@ -9,21 +9,40 @@ dir()
 
 ## Generate some tasks
 cl_name <- "cluster"
-n <- 25
+n <- 10
 x <- seq_len(n)
 x <- as.list(x)
 f <- function(num) {
         pid <- Sys.getpid()
         cat("PID ", pid, " is running task ", num, "\n")
         Sys.sleep(1)
-        list(output = paste0(pid, " is finished running ", num, "!"))
+        paste0(pid, " is finished running ", num, "!")
 }
 
 ## Start up cluster
+cl <- cluster_create(cl_name)
+jq <- cl$jobqueue
+is_empty_input(jq)
+is_empty_output(jq)
+cluster_add_tasks(cl, x, f)
+threadpool:::exportEnv(cl, globalenv())
+peek(cl$jobqueue)
+
+jt <- cluster_next_task(cl)
+jq$queue$list()
+result <- threadpool:::task_run(jt$value, globalenv())
+shelf2output(jq, jt$key, result)
+is_empty_input(jq)
+is_empty_output(jq)
+shelf_list(jq)
+jq$queue$list()
+dequeue(jq)
+jq$queue$list()
+
 cluster_initialize(cl_name, x, f, env = globalenv())
 cl <- cluster_join(cl_name)
-peek(cl$jobqueue)
 cluster_run(cl)
+
 
 r <- cluster_reduce(cl)
 
