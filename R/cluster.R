1a08f9f91bcff027a8730c4c5134b61dfbf8e6c6
Roger D. Peng <rdpeng@gmail.com>
2018-06-15 10:09:47 +1000

    Remove docs for unexported functions

--- R/cluster.R
+++ R/cluster.R
@@ -1,22 +1,10 @@
 
-#' Return cluster paths
-#'
-#' The cluster 'name' ends up being a directory on the filesystem. Then, there
-#' separate files for the queues and evaluation environment.
-#'
-#' @param name cluster name
 cluster_paths <- function(name) {
         list(jobqueue = file.path(name, sprintf("%s.q", basename(name))),
              logfile = logfile_create(name),
              env = file.path(name, sprintf("%s.env.rds", basename(name))))
 }
 
-#' Create a Cluster
-#'
-#' Create the input job queue, the output job queue, and other cluster elements
-#'
-#' @param name the name of the cluster
-#'
 #' @importFrom queue create_job_queue
 #'
 cluster_create <- function(name) {
@@ -34,13 +22,6 @@ new_task <- function(data, func) {
 }
 
 
-#' Retrieve the Next Task
-#'
-#' Retrieve the next task in the input queue for a cluster
-#'
-#' @param cl a cluster object
-#'
-#' @return a task object
 #' @importFrom queue input2shelf
 #'
 cluster_next_task <- function(cl) {
@@ -63,17 +44,8 @@ task_run <- function(task, envir) {
         result
 }
 
-#' Finish a Cluster Task
-#'
-#' Take the output from running a task and add it to the output queue
-#'
-#' @param cl a cluster object
-#' @param job_task a job_task object from the shelf
-#' @param output the output from a task
-#'
 #' @importFrom queue shelf2output
 #'
-
 cluster_finish_task <- function(cl, job_task, output) {
         job_q <- cl$jobqueue
         shelf2output(job_q, job_task$key, output)
